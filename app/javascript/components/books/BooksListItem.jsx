927c060b352b161685d002a411704ae52e75ccb3
a-prostakov <a.prostakov@itransition.com>
2021-06-12 13:30:25 +0300

    Allowed to browse using mouseclick

--- app/javascript/components/books/BooksListItem.jsx
+++ app/javascript/components/books/BooksListItem.jsx
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import classnames from 'classnames'
 
 import ImageContainer from 'components/ImageContainer'
+import { setCurrentBookId } from 'store/actions'
 import { selectBook, selectBookDefaultImageUrl } from 'store/selectors'
 import { useUrlStore } from 'store/urlStore'
 
@@ -14,7 +15,7 @@ const BooksListItem = (props) => {
   const defaultCoverUrl = useSelector(selectBookDefaultImageUrl())
   const coverUrl = book.coverUrl || defaultCoverUrl
   return (
-    <div className={ classnames('book-case') }>
+    <div className='book-case non-selected' onClick={ () => dispatch(setCurrentBookId(id)) } title={ book.title }>
       <ImageContainer className='book-cover' url={ coverUrl }>
       </ImageContainer>
     </div>
