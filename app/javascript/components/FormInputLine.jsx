daf6f589e6b9960dd783b750bd4deacd84453668
a-prostakov <a.prostakov@itransition.com>
2021-12-14 01:49:43 +0100

    Fixed input autofocus

--- app/javascript/components/FormInputLine.jsx
+++ app/javascript/components/FormInputLine.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Col, Form, Row } from 'react-bootstrap'
 import PropTypes from 'prop-types'
 
 const FormInputLine = (props) => {
-  const { controlId, label, value, autoFocus, readOnly, onChange } = props
+  const { controlId, label, value, autoFocus, ...other } = props
   const errors = props.errors || []
+  const ref = useRef(null)
+
+  useEffect(() => autoFocus && ref.current.focus(), [])
+
   return (
     <Form.Group as={ Row } controlId={ controlId }>
       <Form.Label column sm={ 3 }>
         { label }
       </Form.Label>
       <Col sm={ 9 }>
-        <Form.Control type='text' defaultValue={ value } isInvalid={ errors.length > 0 }
-          { ...{ autoFocus, readOnly, onChange } }
+        <Form.Control type='text' defaultValue={ value } isInvalid={ errors.length > 0 } ref={ ref }
+          { ...{ ...other } }
           autoComplete='off'
         />
         <Form.Control.Feedback type='invalid' tooltip>
