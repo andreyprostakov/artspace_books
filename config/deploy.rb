943d388908dd9e8ba0ba0e9bdb4471996890159c
a-prostakov <a.prostakov@itransition.com>
2022-01-02 07:54:14 +0100

    Added favicon; Updated deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ lock "~> 3.16.0"
 set :application, "infospace_books"
 set :repo_url, "git@github.com:andreyprostakov/infospace_books.git"
 set :passenger_restart_with_touch, true
+set :keep_releases, 2
 
 append :linked_files,
        'config/master.key'
