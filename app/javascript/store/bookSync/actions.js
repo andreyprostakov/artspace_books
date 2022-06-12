cd81258c0ea012db7fb5b4e5a4baf2d9faef9e73
Andrey Prostakov <a.prostakov@itransition.com>
2022-06-12 21:29:36 +0200

    Updated stats sync - show pts diff

--- app/javascript/store/bookSync/actions.js
+++ app/javascript/store/bookSync/actions.js
@@ -1,6 +1,7 @@
 import { slice } from 'store/bookSync/slice'
 import { selectBookIdsInProcessing } from 'store/bookSync/selectors'
 import { selectCurrentBookId } from 'store/axis/selectors'
+import { selectBook } from 'store/metadata/selectors'
 import { addBook } from 'store/metadata/actions'
 import apiClient from 'serverApi/apiClient'
 import { addErrorMessage, addSuccessMessage } from 'widgets/notifications/actions'
@@ -11,25 +12,28 @@ const {
 } = slice.actions
 
 export const syncBookStats = id => (dispatch, getState) => {
-  const ids = selectBookIdsInProcessing()(getState())
+  const state = getState()
+  const ids = selectBookIdsInProcessing()(state)
   if (ids.includes(id)) return
 
+  const initialBook = selectBook(id)(state)
   dispatch(markBookAsInProcess(id))
-  dispatch(reloadBookWithSync(id)).then(book => {
+  dispatch(reloadBookWithSync(id, initialBook)).then(book => {
     dispatch(addBook(book))
     dispatch(unmarkBookAsInProcess(id))
   })
 }
 
-const reloadBookWithSync = id => async dispatch => {
+const reloadBookWithSync = (id, initialBook) => async dispatch => {
   const apiCall = apiClient.syncBookStats(id).
     fail(response => {
       dispatch(addErrorMessage(`Book #${id} not synced due to some failure`))
       return response
     }).
-    then(book => {
-      dispatch(addSuccessMessage(`Book #${id} synced`))
-      return book
+    then(reloadedBook => {
+      const diff = reloadedBook.popularity - initialBook.popularity
+      dispatch(addSuccessMessage(`Book #${id} synced! Difference: ${diff}pts`))
+      return reloadedBook
     })
   const result = await Promise.race(
     [
